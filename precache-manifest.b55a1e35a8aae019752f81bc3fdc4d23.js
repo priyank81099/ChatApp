240cd9b07295ca52a3f56ac343214014c58d2373
priyank satasiya <priyanksatasiya81099@gmail.com>
2019-06-26 09:14:07 +0530

    Updates

---
+++ precache-manifest.b55a1e35a8aae019752f81bc3fdc4d23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ad3a43e1329359b34b70a4b4ad3639",
+    "url": "/ChatApp/index.html"
+  },
+  {
+    "revision": "d0e05e31eb8938d7d1f0",
+    "url": "/ChatApp/static/css/main.8180bb62.chunk.css"
+  },
+  {
+    "revision": "48b3adbf206e8c8aa6ca",
+    "url": "/ChatApp/static/js/2.534a26bc.chunk.js"
+  },
+  {
+    "revision": "d0e05e31eb8938d7d1f0",
+    "url": "/ChatApp/static/js/main.8d97d9a9.chunk.js"
+  },
+  {
+    "revision": "10535b7305eede8feba3",
+    "url": "/ChatApp/static/js/runtime~main.805a290e.js"
+  }
+]);
